029c54874547dbe72ee4cca67dbcc5d12228de4d
Mauricio Murcia Spencer <maomur@gmail.com>
2026-03-28 11:48:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3545952382-3e4dc",
+  "appId": "1:86070626860:web:2267cb74b1bf366b3b9f89",
+  "apiKey": "AIzaSyBe2F3X5RBrr7bwhTE3a-Ffa4emy0wExjY",
+  "authDomain": "studio-3545952382-3e4dc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "86070626860"
+};
