0a56edfd5097e2185d047d47c3defc97d303498b
Mauricio Murcia Spencer <maomur@gmail.com>
2026-03-28 17:36:54 +0000

    Try fixing this error: `Runtime ReferenceError: Input is not defined. Er

--- src/components/prompt-page.tsx
+++ src/components/prompt-page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useState, useMemo, useCallback, useEffect } from 'react';
@@ -7,6 +6,7 @@ import { promptCategories } from '@/lib/definitions';
 import Header from '@/components/header';
 import PromptList from '@/components/prompt-list';
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import {
   Dialog,
   DialogContent,
